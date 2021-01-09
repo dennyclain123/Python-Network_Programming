59aaae305f22b1dbc254aef33f754f3f7d163ae6
dennyclain123 <dennyclain123@gmail.com>
2021-01-09 16:32:09 +0630

    Version 2

---
+++ My_PyScripts/Subnet_GUI/test.py
@@ -0,0 +1,12 @@
+from python3-tk import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
