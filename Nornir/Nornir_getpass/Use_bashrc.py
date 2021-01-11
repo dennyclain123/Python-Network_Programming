d853980311607d1f1fc4c61e035b6696fc6d6099
dennyclain123 <dennyclain123@gmail.com>
2021-01-11 17:10:59 +0630

    New Codes

--- Nornir/Nornir_getpass/Use_bashrc.py
+++ Nornir/Nornir_getpass/Use_bashrc.py
@@ -2,13 +2,14 @@ from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_netmiko import netmiko_send_command
 import getpass
+import sys
+import os
 
-username = input("Enter the default username: ")
 nr = InitNornir(config_file="config.yaml")
 
-nr.inventory.defaults.username = username
-password = getpass.getpass()
-nr.inventory.defaults.password = password
+nr.inventory.defaults.username = os.environ["NIT_USER"]
+nr.inventory.defaults.password = os.environ["NIT_PASS"]
+
 def show_command(task):
     task.run(netmiko_send_command, command_string="show ip int br")
 
--- Nornir/Nornir_getpass/using_sys.argv.py
+++ Nornir/Nornir_getpass/using_sys.argv.py
@@ -2,11 +2,11 @@ from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_netmiko import netmiko_send_command
 import getpass
+import sys
 
-username = input("Enter the default username: ")
 nr = InitNornir(config_file="config.yaml")
 
-nr.inventory.defaults.username = username
+nr.inventory.defaults.username = sys.argv[1]
 password = getpass.getpass()
 nr.inventory.defaults.password = password
 def show_command(task):
